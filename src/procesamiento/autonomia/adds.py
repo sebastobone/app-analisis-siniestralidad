fc729f16e25c2b0a83b530b4f8d45900cd9d6eda
sebastobone <sebastiantobon@sura.com.co>
2024-12-10 23:15:00 -0500

    fix: Columnas de compania y ramo como texto en excel autonomia

--- src/procesamiento/autonomia/adds.py
+++ src/procesamiento/autonomia/adds.py
@@ -1,19 +1,26 @@
 import polars as pl
 from controles_informacion.controles_informacion import read_sap
 import constantes as ct
-import xlsxwriter
+import xlwings as xw
+import pandas as pd
+import os
 
 df = read_sap(
     ["Generales", "Vida"], ["pago_cedido", "aviso_cedido"], int(ct.PARAMS_FECHAS[1][1])
 ).filter(pl.col("mes_mov") == int(ct.PARAMS_FECHAS[1][1])).collect()
 
-with xlsxwriter.Workbook("data/segmentacion_autonomia.xlsx") as wb:
-    df.write_excel(workbook=wb, worksheet="add_s_SAP_Sinis_Ced")
+xw.Book(f"{os.getcwd()}/data/segmentacion_autonomia.xlsx").set_mock_caller()
+wb = xw.Book.caller()
 
-# xw.Book(f"{os.getcwd()}/data/segmentacion_autonomia.xlsx").set_mock_caller()
-# wb = xw.Book.caller()
-# wb.sheets.add(name="add_s_SAP_Sinis_Ced")
-# wb.sheets["add_s_SAP_Sinis_Ced"]["A1"].options(index=False).value = df
+try:
+    wb.sheets.add(name="add_s_SAP_Sinis_Ced")
+except ValueError:
+    wb.sheets["add_s_SAP_Sinis_Ced"].delete()
+    wb.sheets.add(name="add_s_SAP_Sinis_Ced")
+
+wb.sheets["add_s_SAP_Sinis_Ced"].range("A1:A500").number_format = "@"
+wb.sheets["add_s_SAP_Sinis_Ced"].range("B1:B500").number_format = "@"
+wb.sheets["add_s_SAP_Sinis_Ced"]["A1"].options(index=False).value = df.to_pandas()
 
 # sap_primas = ctrl.read_sap(['Generales', 'Vida'], ['PRIMAS_EMI', 'PRIMAS_DEV', 'RPND'], ['BRUTO', 'RETENIDO', 'CEDIDO'], int(mes_corte))
 # sap_primas = sap_primas[sap_primas['Mes_Mov'] == int(mes_corte)]

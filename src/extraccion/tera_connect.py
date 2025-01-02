8f66165c646e70401205165c195a99b834517259
sebastobone <sebastiantobon@sura.com.co>
2025-01-02 09:31:06 -0500

    feat: Añadir nuevos tests para tera_connect

--- src/extraccion/tera_connect.py
+++ src/extraccion/tera_connect.py
@@ -32,18 +32,18 @@ def preparar_queries(
 
 
 def cargar_segmentaciones(archivo_segm: str, tipo_query: str) -> list[pl.DataFrame]:
-    segm_sheets = [
-        str(sheet)
-        for sheet in pd.ExcelFile(archivo_segm).sheet_names
-        if str(sheet).startswith("add")
+    hojas_segm = [
+        str(hoja)
+        for hoja in pd.ExcelFile(archivo_segm).sheet_names
+        if str(hoja).startswith("add")
     ]
-    if len(segm_sheets) > 0:
-        check_adds_segmentacion(segm_sheets)
+    if hojas_segm:
+        check_adds_segmentacion(hojas_segm)
+
+    hojas_query = [hoja for hoja in hojas_segm if tipo_query[0] in hoja.split("_")[1]]
 
     return [
-        pl.read_excel(archivo_segm, sheet_name=str(segm_sheet))
-        for segm_sheet in segm_sheets
-        if tipo_query[0] in segm_sheet.split("_")[1]
+        pl.read_excel(archivo_segm, sheet_name=hoja_query) for hoja_query in hojas_query
     ]
 
 
@@ -70,10 +70,11 @@ def fechas_chunks(mes_inicio: int, mes_corte: int) -> list[tuple[date, date]]:
 
 def ejecutar_queries(
     queries: list[str],
-    con: td.TeradataConnection,
+    creds: dict[str, str],
     fechas_chunks: list[tuple[date, date]],
     segm: list[pl.DataFrame],
 ) -> pl.DataFrame:
+    con = td.connect(json.dumps(creds))
     cur = con.cursor()
     add_num = 0
     for query in tqdm(queries):
@@ -108,7 +109,7 @@ def guardar_resultados(
     df = pl.DataFrame(utils.lowercase_columns(df))
 
     if tipo_query != "otro":
-        checks_final_info(tipo_query, df, negocio)
+        check_final_info(tipo_query, df, negocio)
 
         df = df.select(
             utils.col_apertura_reservas(negocio),
@@ -128,7 +129,7 @@ def check_adds_segmentacion(segm_sheets: list[str]) -> None:
     for sheet in segm_sheets:
         partes = sheet.split("_")
         if (len(partes) < 3) or not any(char in partes[1] for char in "spe"):
-            raise ValueError(
+            logger.error(
                 """
                 El nombre de las hojas con tablas a cargar debe seguir el
                 formato "add_[indicador de queries que la utilizan]_[nombre de la tabla]".
@@ -140,14 +141,16 @@ def check_adds_segmentacion(segm_sheets: list[str]) -> None:
                 Corregir el nombre de la hoja.
                 """
             )
+            raise ValueError
 
 
 def check_suficiencia_adds(file: str, queries: str, adds: list[pl.DataFrame]) -> None:
     num_adds_necesarios = queries.count("?);")
     if num_adds_necesarios != len(adds):
-        raise ValueError(
+        logger.error(
             f"""
-            Necesita {num_adds_necesarios} tablas adicionales para ejecutar el query {file},
+            Necesita {num_adds_necesarios} tablas adicionales para 
+            ejecutar el query {file},
             pero en el Excel "segmentacion.xlsx" hay {len(adds)} hojas
             de este tipo. Por favor, revise las hojas que tiene o revise que el 
             nombre de las hojas siga el formato
@@ -155,13 +158,14 @@ def check_suficiencia_adds(file: str, queries: str, adds: list[pl.DataFrame]) ->
             Hojas actuales: {adds}
             """
         )
+        raise ValueError
 
 
 def check_numero_columnas_add(query: str, add: pl.DataFrame) -> None:
     num_cols = query.count("?")
     num_cols_add = len(add.collect_schema().names())
     if num_cols != num_cols_add:
-        raise ValueError(
+        logger.error(
             f"""
             Error en {query}:
             La tabla creada en Teradata recibe {num_cols} columnas, pero la
@@ -171,6 +175,7 @@ def check_numero_columnas_add(query: str, add: pl.DataFrame) -> None:
             sea el mismo que el del query.
             """
         )
+        raise ValueError
 
 
 def check_duplicados(add: pl.DataFrame) -> pl.DataFrame:
@@ -188,32 +193,34 @@ def check_duplicados(add: pl.DataFrame) -> pl.DataFrame:
 def check_nulls(add: pl.DataFrame) -> None:
     num_nulls = add.null_count().max_horizontal().max()
     if isinstance(num_nulls, int) and num_nulls > 0:
-        raise ValueError(
+        logger.error(
             f"""
             Error -> tiene valores nulos en la siguiente tabla: {add}
             Corrija estos valores antes de ejecutar el proceso.
             """
         )
+        raise ValueError
 
 
-def checks_final_info(tipo_query: str, df: pl.DataFrame, negocio: str) -> None:
+def check_final_info(tipo_query: str, df: pl.DataFrame, negocio: str) -> None:
     """Esta funcion se usa cuando se ejecuta un query de siniestros,
     primas, o expuestos que consolida la informacion necesaria para
     pasar a las transformaciones de la plantilla, sin necesidad de
-    hacer procesamiento extra.
+    hacer procesamiento extra. Valida la consistencia de la informacion.
     """
+    cols = df.collect_schema().names()
+
     for column in ct.min_cols_tera(tipo_query):
-        if column not in df.collect_schema().names():
+        if column not in cols:
             raise ValueError(f"""¡Falta la columna {column}!""")
 
-    if (
-        "fecha_siniestro" in df.collect_schema().names()
-        and df.get_column("fecha_siniestro").dtype != pl.Date
-    ):
-        raise ValueError("""La columna fecha_siniestro debe estar en formato fecha.""")
+    if "fecha_siniestro" in cols and df.get_column("fecha_siniestro").dtype != pl.Date:
+        logger.error("""La columna fecha_siniestro debe estar en formato fecha.""")
+        raise ValueError
 
     if df.get_column("fecha_registro").dtype != pl.Date:
-        raise ValueError("""La columna fecha_registro debe estar en formato fecha.""")
+        logger.error("""La columna fecha_registro debe estar en formato fecha.""")
+        raise ValueError
 
     # Segmentaciones faltantes
     df_faltante = df.filter(
@@ -225,11 +232,12 @@ def checks_final_info(tipo_query: str, df: pl.DataFrame, negocio: str) -> None:
         )
     )
     if not df_faltante.is_empty():
-        print(df_faltante)
-        raise ValueError("""¡Alerta! Revise las segmentaciones faltantes.""")
+        logger.error(f"""¡Alerta! Revise las segmentaciones faltantes. {df_faltante}""")
+        raise ValueError
 
 
 def correr_query(
+    creds: dict[str, str],
     file: str,
     save_path: str,
     save_format: str,
@@ -243,12 +251,11 @@ def correr_query(
     segm = cargar_segmentaciones(archivo_segm, tipo)
 
     fchunks = fechas_chunks(mes_inicio, mes_corte)
-    con = td.connect(json.dumps(ct.CREDENCIALES_TERADATA))
 
     queries = preparar_queries(
         open(file).read(), mes_inicio, mes_corte, aproximar_reaseguro
     )
     check_suficiencia_adds(file, queries, segm)
 
-    df = ejecutar_queries(queries.split(";"), con, fchunks, segm)
+    df = ejecutar_queries(queries.split(";"), creds, fchunks, segm)
     guardar_resultados(df, negocio, save_path, save_format, tipo)
--- tests/test_tera_connect.py
+++ tests/test_tera_connect.py
@@ -1,17 +1,20 @@
 from datetime import timedelta
+from unittest.mock import MagicMock, patch
 
 import polars as pl
 import pytest
+from src import constantes as ct
 from src import utils
 from src.app import Parametros
 from src.extraccion import tera_connect
 
 
+@pytest.mark.unit
 def test_tipo_query():
     assert tera_connect.tipo_query("path/to/siniestros.sql") == "siniestros"
     assert tera_connect.tipo_query("path/to/siniestros_bruto.sql") == "otro"
 
-
+@pytest.mark.unit
 def test_preparar_queries(params: Parametros):
     mock_query = """
         SELECT
@@ -39,7 +42,7 @@ def test_preparar_queries(params: Parametros):
 
     assert test == correct_result
 
-
+@pytest.mark.unit
 def test_fechas_chunks(params: Parametros):
     test = tera_connect.fechas_chunks(params.mes_inicio, params.mes_corte)
     print(test[0])
@@ -50,59 +53,87 @@ def test_fechas_chunks(params: Parametros):
 
     assert test[0] == (utils.yyyymm_to_date(params.mes_inicio), mes_inicio_last_day)
 
+@pytest.mark.unit
+@patch("src.extraccion.tera_connect.pd.ExcelFile")
+@patch("src.extraccion.tera_connect.pl.read_excel")
+def test_cargar_segmentaciones(mock_read_excel: MagicMock, mock_excel_file: MagicMock):
+    mock_excel_file.return_value.sheet_names = [
+        "add_s_Amparos",
+        "add_pe_Canales",
+    ]
 
+    mock_read_excel.return_value = pl.DataFrame({"col1": [1, 2, 3]})
+
+    result = tera_connect.cargar_segmentaciones("test_file.xlsx", "siniestros")
+
+    assert len(result) == 1
+    mock_read_excel.assert_called_once_with(
+        "test_file.xlsx", sheet_name="add_s_Amparos"
+    )
+    mock_excel_file.assert_called_once_with("test_file.xlsx")
+
+@pytest.mark.unit
 def test_check_adds_segmentacion():
-    with pytest.raises(ValueError) as exc_info:
+    with pytest.raises(ValueError):
         tera_connect.check_adds_segmentacion(["add_d_Siniestros"])
-    print(exc_info.value)
 
-    with pytest.raises(ValueError) as exc_info:
+    with pytest.raises(ValueError):
         tera_connect.check_adds_segmentacion(["add_Siniestros"])
-    print(exc_info.value)
 
-    assert tera_connect.check_adds_segmentacion(["add_s_Siniestros"]) is None
+    tera_connect.check_adds_segmentacion(["add_s_Siniestros"])
 
-
+@pytest.mark.unit
 def test_check_suficiencia_adds():
     mock_query = """
-
     INSERT INTO POLIZAS VALUES (?, ?);
     INSERT INTO SUCURSALES VALUES (?, ?);
     INSERT INTO CANALES VALUES (?, ?);
-
     """
 
-    with pytest.raises(ValueError) as exc_info:
-        tera_connect.check_suficiencia_adds("siniestros", mock_query, ["add_d_Polizas"])
-    print(exc_info.value)
+    with pytest.raises(ValueError):
+        tera_connect.check_suficiencia_adds("_", mock_query, [pl.DataFrame()])
 
-    with pytest.raises(ValueError) as exc_info:
-        tera_connect.check_suficiencia_adds(
-            "siniestros", mock_query, ["add_s_Polizas", "add_s_Sucursales"]
-        )
-    print(exc_info.value)
+    tera_connect.check_suficiencia_adds("_", mock_query, [pl.DataFrame()] * 3)
 
-    with pytest.raises(ValueError) as exc_info:
-        tera_connect.check_suficiencia_adds(
-            "siniestros",
-            mock_query,
-            ["add_s_Polizas", "add_s_Sucursales", "add_s_Canales", "add_s_Random"],
-        )
-    print(exc_info.value)
-
-    assert (
-        tera_connect.check_suficiencia_adds(
-            "siniestros",
-            mock_query,
-            ["add_s_Polizas", "add_s_Sucursales", "add_s_Canales"],
-        )
-        is None
-    )
-
-
+@pytest.mark.unit
 def test_check_numero_columnas_add():
-    mock_add = pl.DataFrame({"datos": [1, 1, 2, 3, 4, 5]})
     mock_query = "INSERT INTO table VALUES (?, ?)"
+    mock_add_malo = pl.DataFrame({"datos": [1, 1, 2]})
+    mock_add_bueno = pl.DataFrame({"datos": [1, 1, 2], "otro": [1, 1, 2]})
+
+    with pytest.raises(ValueError):
+        tera_connect.check_numero_columnas_add(mock_query, mock_add_malo)
+
+    tera_connect.check_numero_columnas_add(mock_query, mock_add_bueno)
+
+@pytest.mark.unit
+def test_check_nulls():
+    mock_add_malo = pl.DataFrame({"datos": [None, 1, 2, 3, 4, 5]})
+    mock_add_bueno = pl.DataFrame({"datos": [1, 1, 2, 3, 4, 5]})
+
+    with pytest.raises(ValueError):
+        tera_connect.check_nulls(mock_add_malo)
+
+    tera_connect.check_nulls(mock_add_bueno)
+
+@pytest.mark.unit
+def test_check_final_info(mock_siniestros: pl.LazyFrame):
+    tipo_query = "siniestros"
+    df = mock_siniestros.collect()
+
     with pytest.raises(ValueError) as exc_info:
-        tera_connect.check_numero_columnas_add(mock_query, mock_add)
-    print(exc_info.value)
+        tera_connect.check_final_info(tipo_query, df.drop("codigo_op"), "mock")
+    assert str(exc_info.value) == """¡Falta la columna codigo_op!"""
+
+    with pytest.raises(ValueError) as exc_info:
+        tera_connect.check_final_info(
+            tipo_query,
+            df.with_columns(pl.col("fecha_siniestro").cast(pl.String)),
+            "mock",
+        )
+
+    df_falt = df.slice(0, 2).with_columns(
+        [pl.lit(None).alias(col) for col in ct.columnas_aperturas("mock")]
+    )
+    with pytest.raises(ValueError) as exc_info:
+        tera_connect.check_final_info(tipo_query, pl.concat([df, df_falt]), "mock")

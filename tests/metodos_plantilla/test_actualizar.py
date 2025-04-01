b4b697889a05353e94b300da9c62b4e69b267f1b
sebastobone <sebastiantobon@sura.com.co>
2025-03-31 23:15:39 -0500

    feat: Agregar tests para modo actualizar

---
+++ tests/metodos_plantilla/test_actualizar.py
@@ -0,0 +1,100 @@
+from datetime import date
+
+import pytest
+from fastapi.testclient import TestClient
+from src.metodos_plantilla import abrir, actualizar
+from tests.conftest import agregar_meses_params, vaciar_directorio
+
+
+def test_actualizar_sin_generar(client: TestClient, rango_meses: tuple[date, date]):
+    params_form = {
+        "negocio": "demo",
+        "tipo_analisis": "triangulos",
+        "nombre_plantilla": "wb_test",
+    }
+    agregar_meses_params(params_form, rango_meses)
+
+    _ = client.post("/ingresar-parametros", data=params_form).json()
+
+    _ = client.post("/correr-query-siniestros")
+    _ = client.post("/correr-query-primas")
+    _ = client.post("/correr-query-expuestos")
+
+    _ = client.post("/preparar-plantilla")
+
+    with pytest.raises(actualizar.PlantillaNoGeneradaError):
+        _ = client.post(
+            "/actualizar-plantilla",
+            data={"apertura": "01_001_A_D", "atributo": "bruto", "plantilla": "plata"},
+        )
+
+    vaciar_directorio("data/raw")
+    vaciar_directorio("data/processed")
+
+
+def test_actualizar_diferentes_periodicidades(
+    client: TestClient, rango_meses: tuple[date, date]
+):
+    params_form = {
+        "negocio": "demo",
+        "tipo_analisis": "triangulos",
+        "nombre_plantilla": "wb_test",
+    }
+    agregar_meses_params(params_form, rango_meses)
+
+    _ = client.post("/ingresar-parametros", data=params_form).json()
+
+    _ = client.post("/correr-query-siniestros")
+    _ = client.post("/correr-query-primas")
+    _ = client.post("/correr-query-expuestos")
+
+    _ = client.post("/preparar-plantilla")
+
+    _ = client.post(
+        "/generar-plantilla",
+        data={"apertura": "01_001_A_D", "atributo": "bruto", "plantilla": "plata"},
+    )
+
+    with pytest.raises(actualizar.PeriodicidadDiferenteError):
+        _ = client.post(
+            "/actualizar-plantilla",
+            data={"apertura": "01_002_A_D", "atributo": "bruto", "plantilla": "plata"},
+        )
+
+    vaciar_directorio("data/raw")
+    vaciar_directorio("data/processed")
+
+
+def test_actualizar_severidad(client: TestClient, rango_meses: tuple[date, date]):
+    params_form = {
+        "negocio": "demo",
+        "tipo_analisis": "triangulos",
+        "nombre_plantilla": "wb_test",
+    }
+    agregar_meses_params(params_form, rango_meses)
+
+    _ = client.post("/ingresar-parametros", data=params_form).json()
+    wb = abrir.abrir_plantilla(f"plantillas/{params_form['nombre_plantilla']}.xlsm")
+
+    _ = client.post("/correr-query-siniestros")
+    _ = client.post("/correr-query-primas")
+    _ = client.post("/correr-query-expuestos")
+
+    _ = client.post("/preparar-plantilla")
+
+    _ = client.post(
+        "/generar-plantilla",
+        data={"apertura": "01_001_A_D", "atributo": "bruto", "plantilla": "severidad"},
+    )
+
+    _ = client.post(
+        "/actualizar-plantilla",
+        data={"apertura": "01_001_A_E", "atributo": "bruto", "plantilla": "severidad"},
+    )
+
+    apertura_en_frecuencia = actualizar.obtener_apertura_actual(wb, "frecuencia")
+    apertura_en_severidad = actualizar.obtener_apertura_actual(wb, "severidad")
+    assert apertura_en_frecuencia == apertura_en_severidad
+
+    vaciar_directorio("data/raw")
+    vaciar_directorio("data/processed")

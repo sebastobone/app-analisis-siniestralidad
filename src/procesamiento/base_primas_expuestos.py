67c274253feec751957ed37b05b2a980ca43b298
sebastobone <sebastiantobon@sura.com.co>
2025-04-07 20:25:25 -0500

    feat: Agregar primas de asistencias a movilidad

--- src/procesamiento/base_primas_expuestos.py
+++ src/procesamiento/base_primas_expuestos.py
@@ -22,6 +22,12 @@ def generar_base_primas_expuestos(
 
     columnas_aperturas = utils.obtener_nombres_aperturas(negocio, qty)
 
+    if negocio == "movilidad" and qty == "primas":
+        primas_asistencias = pl.read_excel(
+            "data/segmentacion_movilidad.xlsx", sheet_name="Primas_asistencias_SAP"
+        ).lazy()
+        df = pl.concat([df, primas_asistencias], how="vertical_relaxed")
+
     df_group = (
         df.with_columns(fechas_pdn(pl.col("fecha_registro")))
         .select(columnas_aperturas + qty_cols + list(ct.PERIODICIDADES.keys()))

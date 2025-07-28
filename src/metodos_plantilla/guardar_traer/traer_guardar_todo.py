7a845aebadc39954f7918d2c74083298896617e3
sebastobone <sebastiantobon@sura.com.co>
2025-07-28 16:21:32 -0500

    feat: Considerar solamente el atributo actual para guardar y traer todo

--- src/metodos_plantilla/guardar_traer/traer_guardar_todo.py
+++ src/metodos_plantilla/guardar_traer/traer_guardar_todo.py
@@ -24,37 +24,31 @@ async def traer_y_guardar_todas_las_aperturas(
         .get_column("apertura_reservas")
         .to_list()
     )
-    atributos = ["bruto", "retenido"] if modos.plantilla != "frecuencia" else ["bruto"]
 
     num_apertura = 0
     for apertura in aperturas:
-        for atributo in atributos:
-            modos_actual = modos.model_copy()
-            modos_actual.apertura = apertura
-            modos_actual.atributo = atributo  # type: ignore
+        modos_actual = modos.model_copy()
+        modos_actual.apertura = apertura
 
-            try:
-                actualizar.actualizar_plantillas(wb, p, modos_actual)
-            except (
-                actualizar.PlantillaNoGeneradaError,
-                actualizar.PeriodicidadDiferenteError,
-            ):
-                generar.generar_plantillas(wb, p, modos_actual)
-            if traer:
-                traer_apertura(wb, modos_actual)
-            guardar_apertura(wb, modos_actual)
+        try:
+            actualizar.actualizar_plantillas(wb, p, modos_actual)
+        except (
+            actualizar.PlantillaNoGeneradaError,
+            actualizar.PeriodicidadDiferenteError,
+        ):
+            generar.generar_plantillas(wb, p, modos_actual)
+        if traer:
+            traer_apertura(wb, modos_actual)
+        guardar_apertura(wb, modos_actual)
 
-            await asyncio.sleep(0)
+        await asyncio.sleep(0)
 
-            logger.info(
-                utils.limpiar_espacios_log(
-                    f"""
-                    Apertura {num_apertura + 1} de {len(aperturas) * len(atributos)}
-                    terminada.
-                    """
-                )
+        logger.info(
+            utils.limpiar_espacios_log(
+                f"Apertura {num_apertura + 1} de {len(aperturas)} terminada."
             )
-            num_apertura += 1
+        )
+        num_apertura += 1
 
     if traer:
         logger.success("Todas las aperturas se han traido y guardado correctamente.")

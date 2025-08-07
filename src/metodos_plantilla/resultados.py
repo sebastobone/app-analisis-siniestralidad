5b4bdedaafb1f505af3a85df75295bb259293870
sebastobone <sebastiantobon@sura.com.co>
2025-08-07 18:36:10 -0500

    fix: Agrupar ocurrencias mensuales en tabla resumen triangulos

--- src/metodos_plantilla/resultados.py
+++ src/metodos_plantilla/resultados.py
@@ -34,13 +34,53 @@ def concatenar_archivos_resultados() -> pl.DataFrame:
             pl.DataFrame(pl.concat(dfs, how="diagonal_relaxed"))
             .unique(subset=columnas_distintivas + ["periodo_ocurrencia"], keep="last")
             .sort(columnas_distintivas + ["periodo_ocurrencia"])
-            .with_columns(pl.col("periodo_ocurrencia").cast(pl.Int32))
+            .pipe(agregar_periodo_granular, columnas_distintivas)
+            .with_columns(
+                pl.col("periodo_ocurrencia", "periodo_granular").cast(pl.Int32)
+            )
         )
     else:
         logger.warning("No se encontraron resultados anteriores.")
         df_resultados = pl.DataFrame()
 
     return df_resultados
+
+
+def agregar_periodo_granular(
+    dfs: pl.DataFrame, columnas_distintivas: list[str]
+) -> pl.DataFrame:
+    granularidades = (
+        dfs.select(columnas_distintivas + ["periodicidad_ocurrencia"])
+        .with_columns(
+            pl.col("periodicidad_ocurrencia").first().over(columnas_distintivas)
+        )
+        .rename({"periodicidad_ocurrencia": "granularidad"})
+        .unique()
+    )
+
+    return dfs.join(
+        granularidades, on=columnas_distintivas, how="left", validate="m:1"
+    ).with_columns(
+        periodo_granular=pl.when(pl.col("granularidad") == "Anual")
+        .then((pl.col("periodo_ocurrencia") // 100) * 100 + 12)
+        .when(pl.col("granularidad") == "Semestral")
+        .then(
+            (pl.col("periodo_ocurrencia") // 100) * 100
+            + pl.when(pl.col("periodo_ocurrencia") % 100 < 7).then(6).otherwise(12)
+        )
+        .when(pl.col("granularidad") == "Trimestral")
+        .then(
+            (pl.col("periodo_ocurrencia") // 100) * 100
+            + pl.when(pl.col("periodo_ocurrencia") % 100 < 4)
+            .then(3)
+            .when(pl.col("periodo_ocurrencia") % 100 < 7)
+            .then(6)
+            .when(pl.col("periodo_ocurrencia") % 100 < 10)
+            .then(9)
+            .otherwise(12)
+        )
+        .otherwise(pl.col("periodo_ocurrencia"))
+    )
 
 
 def actualizar_wb_resultados() -> xw.Book:
